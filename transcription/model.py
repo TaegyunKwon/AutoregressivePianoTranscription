522ac569676283a0051793214fce5fb4bc14b2f3
Taegyun Kwon <ilcobo2@kaist.ac.kr>
2023-10-17 18:16:29 +0900

    HPP_Conv

--- transcription/model.py
+++ transcription/model.py
@@ -25,7 +25,7 @@ class ARModel(nn.Module):
         elif 'Mel2' in self.model:
             self.frontend = CombinedSpec()
         elif 'MIDI' in self.model:
-            self.frontend = MIDIFrontEnd()
+            self.frontend = MIDIFrontEnd(n_per_pitch=4)
         else:
             self.frontend = transforms.MelSpectrogram(sample_rate=SR, n_fft=config.n_fft,
                 hop_length=HOP, f_min=config.f_min, f_max=config.f_max, n_mels=config.n_mels, normalized=False)
@@ -56,6 +56,13 @@ class ARModel(nn.Module):
                                 config.win_fw, config.win_bw, config.hidden_per_pitch,
                                 use_film=config.film, cnn_widths=config.cnn_widths, multifc=config.multifc)
             self.vel_acoustic = PAR_v2_midi(config.n_mels, config.cnn_unit, config.fc_unit, 
+                                config.win_fw, config.win_bw, config.hidden_per_pitch,
+                                use_film=config.film, cnn_widths=config.cnn_widths, multifc=config.multifc)
+        elif self.model == 'PAR_v2_MIDI_HPP':
+            self.acoustic = PAR_v2_HPP(config.n_mels, config.cnn_unit, config.fc_unit, 
+                                config.win_fw, config.win_bw, config.hidden_per_pitch,
+                                use_film=config.film, cnn_widths=config.cnn_widths, multifc=config.multifc)
+            self.vel_acoustic = PAR_v2_HPP(config.n_mels, config.cnn_unit, config.fc_unit, 
                                 config.win_fw, config.win_bw, config.hidden_per_pitch,
                                 use_film=config.film, cnn_widths=config.cnn_widths, multifc=config.multifc)
         elif self.model == 'PC_v8':
@@ -831,6 +838,163 @@ class PAR_v2_midi(nn.Module):
         # pitchwise_x = pitchwise_x.view(batch_size, 88, -1, self.hidden_per_pitch)
         # pitchwise_x = pitchwise_x.permute(0, 2, 3, 1) # B T H 88
         return F.relu(self.layernorm(pitchwise_x))
+
+class HarmonicDilatedConv(nn.Module):
+    def __init__(self, c_in, c_out) -> None:
+        super().__init__()
+        self.conv_1 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 48])
+        self.conv_2 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 76])
+        self.conv_3 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 96])
+        self.conv_4 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 111])
+        self.conv_5 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 124])
+        self.conv_6 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 135])
+        self.conv_7 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 144])
+        self.conv_8 = nn.Conv2d(c_in, c_out, [1, 3], padding='same', dilation=[1, 152])
+    def forward(self, x):
+        x = self.conv_1(x) + self.conv_2(x) + self.conv_3(x) + self.conv_4(x) +\
+            self.conv_5(x) + self.conv_6(x) + self.conv_7(x) + self.conv_8(x)
+        x = th.relu(x)
+        return x
+
+class PAR_v2_HPP(nn.Module):
+    def get_conv2d_block(self, channel_in,channel_out, kernel_size = [1, 3], pool_size = None, dilation = [1, 1]):
+        if(pool_size == None):
+            return nn.Sequential( 
+                nn.Conv2d(channel_in, channel_out, kernel_size=kernel_size, padding='same', dilation=dilation),
+                nn.ReLU(),
+                # nn.BatchNorm2d(channel_out),
+                nn.InstanceNorm2d(channel_out),
+                
+            )
+        else:
+            return nn.Sequential( 
+                nn.Conv2d(channel_in, channel_out, kernel_size=kernel_size, padding='same', dilation=dilation),
+                nn.ReLU(),
+                nn.MaxPool2d(pool_size),
+                # nn.BatchNorm2d(channel_out),
+                nn.InstanceNorm2d(channel_out)
+            )
+
+    # SimpleConv without Pitchwise Conv
+    def __init__(self, n_mels, cnn_unit, fc_unit, win_fw, win_bw, hidden_per_pitch, use_film,
+                 cnn_widths = [3,3,3,3,3,3], multifc=True):
+        super().__init__()
+
+        self.win_fw = win_fw
+        self.win_bw = win_bw
+        self.hidden_per_pitch = hidden_per_pitch
+        # input is batch_size * 1 channel * frames * 700
+        self.block_1 = self.get_conv2d_block(3, cnn_unit, kernel_size=7)
+        self.block_2 = self.get_conv2d_block(cnn_unit, cnn_unit, kernel_size=7)
+        self.block_2_5 = self.get_conv2d_block(cnn_unit, cnn_unit, kernel_size=7)
+
+        c3_out = 128
+        
+        self.conv_3 = HarmonicDilatedConv(cnn_unit, c3_out)
+
+        self.block_4 = self.get_conv2d_block(c3_out, c3_out, pool_size=[1, 4], dilation=[1, 48])
+        self.block_5 = self.get_conv2d_block(c3_out, c3_out, dilation=[1, 12])
+        self.block_6 = self.get_conv2d_block(c3_out, c3_out, [5,1])
+        self.block_7 = self.get_conv2d_block(c3_out, c3_out, [5,1])
+        self.block_8 = self.get_conv2d_block(c3_out, c3_out, [5,1])
+
+    def forward(self, mel):
+        x = self.block_1(mel.permute(0, 1, 3, 2))
+        x = self.block_2(x)
+        x = self.block_2_5(x)
+        x = self.conv_3(x)
+        x = self.block_4(x)
+        x = x[:,:,:,:88]
+        # => [b x 1 x T x 88]
+
+        x = self.block_5(x)
+        # => [b x ch x T x 88]
+        x = self.block_6(x) # + x
+        x = self.block_7(x) # + x
+        x = self.block_8(x) # + x
+        # x = self.conv_9(x)
+        # x = torch.relu(x)
+        # x = self.conv_10(x)
+        # x = torch.sigmoid(x)
+        
+        x = x.permute(0, 2, 1, 3)  # B, 128, T, 88 -> B, T, 128, 88
+        return x  
+
+
+
+class CNNTrunk(nn.Module):
+    def get_conv2d_block(self, channel_in,channel_out, kernel_size = [1, 3], pool_size = None, dilation = [1, 1]):
+        if(pool_size == None):
+            return nn.Sequential( 
+                nn.Conv2d(channel_in, channel_out, kernel_size=kernel_size, padding='same', dilation=dilation),
+                nn.ReLU(),
+                # nn.BatchNorm2d(channel_out),
+                nn.InstanceNorm2d(channel_out),
+                
+            )
+        else:
+            return nn.Sequential( 
+                nn.Conv2d(channel_in, channel_out, kernel_size=kernel_size, padding='same', dilation=dilation),
+                nn.ReLU(),
+                nn.MaxPool2d(pool_size),
+                # nn.BatchNorm2d(channel_out),
+                nn.InstanceNorm2d(channel_out)
+            )
+
+    def __init__(self, c_in = 1, c_har = 16,  embedding = 128, fixed_dilation = 24) -> None:
+        super().__init__()
+
+        self.block_1 = self.get_conv2d_block(c_in, c_har, kernel_size=7)
+        self.block_2 = self.get_conv2d_block(c_har, c_har, kernel_size=7)
+        self.block_2_5 = self.get_conv2d_block(c_har, c_har, kernel_size=7)
+
+        c3_out = embedding
+        
+        self.conv_3 = HarmonicDilatedConv(c_har, c3_out)
+
+        self.block_4 = self.get_conv2d_block(c3_out, c3_out, pool_size=[1, 4], dilation=[1, 48])
+        self.block_5 = self.get_conv2d_block(c3_out, c3_out, dilation=[1, 12])
+        self.block_6 = self.get_conv2d_block(c3_out, c3_out, [5,1])
+        self.block_7 = self.get_conv2d_block(c3_out, c3_out, [5,1])
+        self.block_8 = self.get_conv2d_block(c3_out, c3_out, [5,1])
+        # self.conv_9 = nn.Conv2d(c3_out, 64,1)
+        # self.conv_10 = nn.Conv2d(64, 1, 1)
+
+    def forward(self, log_gram_db):
+        # inputs: [b x 2 x T x n_freq] , [b x 1 x T x 88]
+        # outputs: [b x T x 88]
+
+
+        # img_path = 'logspecgram_preview.png'
+        # if not os.path.exists(img_path):
+        #     img = torch.permute(log_gram_db, [2, 0, 1]).reshape([352, 640*4]).detach().cpu().numpy()
+        #     # x_grid = torchvision.utils.make_grid(x.swapaxes(0, 1), pad_value=1.0).swapaxes(0, 2).detach().cpu().numpy()
+        #     # plt.imsave(img_path, (x_grid+80)/100)
+        #     plt.imsave(img_path, img)
+
+        # => [b x 1 x T x 352]
+        # x = torch.unsqueeze(log_gram_db, dim=1)
+
+
+
+        x = self.block_1(log_gram_db)
+        x = self.block_2(x)
+        x = self.block_2_5(x)
+        x = self.conv_3(x)
+        x = self.block_4(x)
+        # => [b x 1 x T x 88]
+
+        x = self.block_5(x)
+        # => [b x ch x T x 88]
+        x = self.block_6(x) # + x
+        x = self.block_7(x) # + x
+        x = self.block_8(x) # + x
+        # x = self.conv_9(x)
+        # x = torch.relu(x)
+        # x = self.conv_10(x)
+        # x = torch.sigmoid(x)
+
+        return x
 
 class PAR_v4_midi(nn.Module):
     # SimpleConv without Pitchwise Conv
